f377fe75d4fd039b398f742bd65dff24c7b3471d
Matanya Loewenthal <matanya@loewenthal.net>
2024-06-20 11:31:12 -0600

    fix: more config

--- sefaria_ai/main.py
+++ sefaria_ai/main.py
@@ -24,16 +24,12 @@ def run():
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
+    st.write("# Welcome to Sefaria.ai! 👋")
 
     st.markdown(
         """
         Streamlit is an open-source app framework built specifically for
         Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
         ### Want to learn more?
         - Check out [streamlit.io](https://streamlit.io)
         - Jump into our [documentation](https://docs.streamlit.io)
